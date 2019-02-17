5fae8dd8df86f6ae40fb8f4d974cc1eba0b76a1f
msftrncs <msftrncs@midlands.net>
2019-02-16 19:50:16 -0600

    more comment fixes

--- tools/build-atomGrammar.ps1
+++ tools/build-atomGrammar.ps1
@@ -1,10 +1,10 @@
 # read in the PList grammar
 $grammar_plist = [xml](Get-Content 'PowerShellSyntax.tmLanguage') | ConvertFrom-Plist
 
-# create an empty atom grammar heading
+# create an empty Atom grammar
 $grammar_atom = [ordered]@{}
 
-# move select items from the PList to the CSON grammar, excluding a couple items
+# move most items from the PList to the Atom grammar, excluding a couple items
 foreach ($key in $grammar_plist.keys) {
     if ($key -cnotin 'uuid', 'keyEquivalent') {
         $grammar_atom += [ordered]@{
