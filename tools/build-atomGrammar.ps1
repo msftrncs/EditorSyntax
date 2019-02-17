eb168f34a9cc7ebc68d585e4be0a036e05ade690
msftrncs <msftrncs@midlands.net>
2019-02-16 19:25:00 -0600

    fix atom build comments

--- tools/build-atomGrammar.ps1
+++ tools/build-atomGrammar.ps1
@@ -1,11 +1,11 @@
 # read in the PList grammar
 $grammar_plist = [xml](Get-Content 'PowerShellSyntax.tmLanguage') | ConvertFrom-Plist
 
-# create the atom grammar heading, including the commit version
+# create an empty atom grammar heading
 $grammar_atom = [ordered]@{}
 
-# move select items from the PList to the JSON grammar in a specific order, if they exist in the PList
-foreach($key in $grammar_plist.keys) {
+# move select items from the PList to the CSON grammar, excluding a couple items
+foreach ($key in $grammar_plist.keys) {
     if ($key -cnotin 'uuid', 'keyEquivalent') {
         $grammar_atom += [ordered]@{
             $key = $grammar_plist.$key
@@ -18,8 +18,6 @@ if (!(Test-Path 'grammars' -PathType Container)) {
     New-Item 'grammars' -ItemType Directory
 }
 
-# output the final JSON grammar (changing the leading double spaces to tabs)
-# Note: on PS <= 5.1, ConvertTo-JSON uses 4 spaces or alignment with the parent property,
-#       PS >= 6.0 uses 2, so the `-replace` only works correctly with PS >= 6.0
+# output the Atom CSON grammar (escaping what appear to be subexpressions)
 ($grammar_atom | ConvertTo-Json -depth 100 | json2cson ) -replace "(?<!\\)(#\{\()", "\`$1" |
     Set-Content 'grammars\powershell.cson'
