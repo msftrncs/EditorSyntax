8b0bbe0cc3b95bebc42fb2f9160b5ad28a13eac1
msftrncs <msftrncs@midlands.net>
2019-03-03 17:17:33 -0600

    error control

--- tools/build-atomGrammar.ps1
+++ tools/build-atomGrammar.ps1
@@ -1,23 +1,29 @@
-# read in the PList grammar
-$grammar_plist = [xml](Get-Content 'PowerShellSyntax.tmLanguage') | ConvertFrom-Plist
+# convert the PowerShell grammar from PLIST to CSON format for use in the Language-PowerShell package for Atom
+try {
+    # create the output folder if it does not exist
+    if (-not (Test-Path 'grammars' -PathType Container)) {
+        New-Item 'grammars' -ItemType Directory | Out-Null
+    }
 
-# create an empty Atom grammar
-$grammar_atom = [ordered]@{}
+    # read in the PList grammar
+    $grammar_plist = [xml](Get-Content 'PowerShellSyntax.tmLanguage') | ConvertFrom-Plist
 
-# move most items from the PList to the Atom grammar, excluding a couple items
-foreach ($key in $grammar_plist.keys) {
-    if ($key -cnotin 'uuid', 'keyEquivalent') {
-        $grammar_atom += [ordered]@{
-            $key = $grammar_plist.$key
+    # create an empty Atom grammar
+    $grammar_atom = [ordered]@{}
+
+    # move most items from the PList to the Atom grammar, excluding a couple items
+    foreach ($key in $grammar_plist.keys) {
+        if ($key -cnotin 'uuid', 'keyEquivalent') {
+            $grammar_atom += [ordered]@{
+                $key = $grammar_plist.$key
+            }
         }
     }
-}
 
-# create the output folder if it does not exist
-if (!(Test-Path 'grammars' -PathType Container)) {
-    New-Item 'grammars' -ItemType Directory | Out-Null
+    # output the Atom CSON grammar
+    $grammar_atom | ConvertTo-Cson -depth 100 |
+        Set-Content 'grammars\powershell.cson' -Encoding 'UTF8'
 }
-
-# output the Atom CSON grammar
-$grammar_atom | ConvertTo-Cson -depth 100 |
-    Set-Content 'grammars\powershell.cson' -Encoding 'UTF8'
+catch {
+    throw # error occured, give it forward to the user
+}
--- tools/build-grammar.ps1
+++ tools/build-grammar.ps1
@@ -1,31 +1,37 @@
-# read in the PList grammar
-$grammar_plist = [xml](Get-Content 'PowerShellSyntax.tmLanguage') | ConvertFrom-Plist
+# convert the PowerShell grammar from PLIST to CSON format for use in the Language-PowerShell package for Atom
+try {
+    # create the output folder if it does not exist
+    if (-not (Test-Path 'grammars' -PathType Container)) {
+        New-Item 'grammars' -ItemType Directory | Out-Null
+    }
 
-# create the JSON grammar heading, including the commit version
-$grammar_json = [ordered]@{
-    information_for_contributors =
-    'This file has been converted from source and may not be in line with the official build.',
-    'The current master branch for this syntax can be found here: https://github.com/PowerShell/EditorSyntax',
-    'If you want to provide a fix or improvement, please create a pull request against the original repository.'
-    version                      = git rev-parse HEAD
-}
+    # read in the PList grammar
+    $grammar_plist = [xml](Get-Content 'PowerShellSyntax.tmLanguage') | ConvertFrom-Plist
 
-# move select items from the PList to the JSON grammar in a specific order, if they exist in the PList
-foreach ($key in 'name', 'scopeName', 'comment', 'injections', 'patterns', 'repository') {
-    if ($grammar_plist.$key) {
-        $grammar_json += [ordered]@{
-            $key = $grammar_plist.$key
+    # create the JSON grammar heading, including the commit version
+    $grammar_json = [ordered]@{
+        information_for_contributors =
+        'This file has been converted from source and may not be in line with the official build.',
+        'The current master branch for this syntax can be found here: https://github.com/PowerShell/EditorSyntax',
+        'If you want to provide a fix or improvement, please create a pull request against the original repository.'
+        version                      = git rev-parse HEAD
+    }
+
+    # move select items from the PList to the JSON grammar in a specific order, if they exist in the PList
+    foreach ($key in 'name', 'scopeName', 'comment', 'injections', 'patterns', 'repository') {
+        if ($grammar_plist.$key) {
+            $grammar_json += [ordered]@{
+                $key = $grammar_plist.$key
+            }
         }
     }
-}
 
-# create the output folder if it does not exist
-if (!(Test-Path 'grammars' -PathType Container)) {
-    New-Item 'grammars' -ItemType Directory | Out-Null
+    # output the final JSON grammar (changing the leading double spaces to tabs)
+    # Note: on PS <= 5.1, ConvertTo-JSON uses 4 spaces or alignment with the parent property,
+    #       PS >= 6.0 uses 2, so the `-replace` only works correctly with PS >= 6.0
+    ($grammar_json | ConvertTo-Json -depth 100) -replace "(?m)  (?<=^(?:  )*)", "`t" |
+        Set-Content 'grammars\powershell.tmLanguage.json' -Encoding 'UTF8'
 }
-
-# output the final JSON grammar (changing the leading double spaces to tabs)
-# Note: on PS <= 5.1, ConvertTo-JSON uses 4 spaces or alignment with the parent property,
-#       PS >= 6.0 uses 2, so the `-replace` only works correctly with PS >= 6.0
-($grammar_json | ConvertTo-Json -depth 100) -replace "(?m)  (?<=^(?:  )*)", "`t" |
-    Set-Content 'grammars\powershell.tmLanguage.json' -Encoding 'UTF8'
+catch {
+    throw # error occured, give it forward to the user
+}
